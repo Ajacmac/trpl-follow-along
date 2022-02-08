16873ef58fb87cf73ed49ae6b1d0ad6e1126bc3f
Jacob MacRitchie <jacob.macritchie@gmail.com>
2022-02-08 09:53:41 -0400

    tried labelled loops. feels weird, but interesting

---
+++ named_loops/target/debug/deps/named_loops.d
@@ -0,0 +1,5 @@
+C:\codeProjects\Rust\trpl-follow-along\named_loops\target\debug\deps\named_loops.exe: src\main.rs
+
+C:\codeProjects\Rust\trpl-follow-along\named_loops\target\debug\deps\named_loops.d: src\main.rs
+
+src\main.rs:
---
+++ named_loops/target/debug/named_loops.d
@@ -0,0 +1,1 @@
+C:\codeProjects\Rust\trpl-follow-along\named_loops\target\debug\named_loops.exe: C:\codeProjects\Rust\trpl-follow-along\named_loops\src\main.rs
