289b24559ada1a6a5dd6220f0898ea6f37d1f2f9
Ajacmac <jacob.macritchie@gmail.com>
2022-03-13 15:08:12 -0300

    first word w/ slices works

--- first_word/src/main.rs
+++ first_word/src/main.rs
@@ -3,14 +3,14 @@ fn main() {
     println!("{}", find_first_no_slices(&test1));
 }
 
-fn find_first_no_slices(s: &String) -> usize {
+fn find_first_no_slices(s: &String) -> &str {
     let bytes = s.as_bytes();
 
     for (i, &item) in bytes.iter().enumerate() {
         if item == b' ' {
-            return i;
+            return &s[..i];
         }
     }
 
-    s.len()
+    &s[..]
 }
