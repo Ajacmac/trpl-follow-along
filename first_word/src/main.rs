e6eadf5872e4874c4ec543b1b52b5cf570515d74
Ajacmac <jacob.macritchie@gmail.com>
2022-03-14 21:26:50 -0300

    improved function signature for flexibility

--- first_word/src/main.rs
+++ first_word/src/main.rs
@@ -1,9 +1,9 @@
 fn main() {
-    let test1 = String::from("test test test");
+    let test1 = "test test test";
     println!("{}", find_first_no_slices(&test1));
 }
 
-fn find_first_no_slices(s: &String) -> &str {
+fn find_first_no_slices(s: &str) -> &str {
     let bytes = s.as_bytes();
 
     for (i, &item) in bytes.iter().enumerate() {
