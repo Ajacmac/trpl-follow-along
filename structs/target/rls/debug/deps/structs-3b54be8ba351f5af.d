9a30004b5f6b577d70690ee3d3c2fb2f8c038633
Ajacmac <jacob.macritchie@gmail.com>
2022-03-15 21:00:50 -0300

    initial version without structs

---
+++ structs/target/rls/debug/deps/structs-3b54be8ba351f5af.d
@@ -0,0 +1,5 @@
+/Users/jacobmacritchie/CodeProjects/trpl-follow-along/structs/target/rls/debug/deps/structs-3b54be8ba351f5af.rmeta: src/main.rs
+
+/Users/jacobmacritchie/CodeProjects/trpl-follow-along/structs/target/rls/debug/deps/structs-3b54be8ba351f5af.d: src/main.rs
+
+src/main.rs:
---
+++ structs/target/rls/debug/deps/structs-d173f9dce6c62b61.d
@@ -0,0 +1,5 @@
+/Users/jacobmacritchie/CodeProjects/trpl-follow-along/structs/target/rls/debug/deps/structs-d173f9dce6c62b61.rmeta: src/main.rs
+
+/Users/jacobmacritchie/CodeProjects/trpl-follow-along/structs/target/rls/debug/deps/structs-d173f9dce6c62b61.d: src/main.rs
+
+src/main.rs:
