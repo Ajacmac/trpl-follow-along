f07bce5ff99d4ad925f4f26b0048823173509efe
Ajacmac <jacob.macritchie@gmail.com>
2022-03-17 21:32:41 -0300

    added second method can_hold()

--- structs/src/main.rs
+++ structs/src/main.rs
@@ -8,24 +8,28 @@ impl Rectangle {
     fn area(&self) -> u32 {
         self.width * self.height
     }
+
+    fn can_hold(&self, other: &Rectangle) -> bool {
+        self.width > other.width && self.height > other.height
+    }
 }
 
 fn main() {
-    let scale = 2;
     let rect1 = Rectangle {
-        width: dbg!(30 * scale),
+        width: 30,
         height: 50,
     };
 
-    println!(
-        "Rect1 is {:#?}.",
-        rect1
-    );
+    let rect2 = Rectangle {
+        width: 10,
+        height: 40,
+    };
 
-    dbg!(&rect1);
+    let rect3 = Rectangle {
+        width: 60,
+        height: 45,
+    };
 
-    println!(
-        "The area of rect1 is {:#?}.",
-        rect1.area()
-    );
+    println!("Can rect1 hold rect2? {}", rect1.can_hold(&rect2));
+    println!("Can rect1 hold rect3? {}", rect1.can_hold(&rect3));
 }
