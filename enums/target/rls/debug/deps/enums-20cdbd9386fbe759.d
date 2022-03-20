e98cebe18290fa7c1b3fed265d3e6397a624083d
Ajacmac <jacob.macritchie@gmail.com>
2022-03-19 21:11:47 -0300

    tinkered with some enum implementations

---
+++ enums/target/rls/debug/deps/enums-20cdbd9386fbe759.d
@@ -0,0 +1,5 @@
+/Users/jacobmacritchie/CodeProjects/trpl-follow-along/enums/target/rls/debug/deps/enums-20cdbd9386fbe759.rmeta: src/main.rs
+
+/Users/jacobmacritchie/CodeProjects/trpl-follow-along/enums/target/rls/debug/deps/enums-20cdbd9386fbe759.d: src/main.rs
+
+src/main.rs:
---
+++ enums/target/rls/debug/deps/enums-32cb1a8ee16c5c9a.d
@@ -0,0 +1,5 @@
+/Users/jacobmacritchie/CodeProjects/trpl-follow-along/enums/target/rls/debug/deps/enums-32cb1a8ee16c5c9a.rmeta: src/main.rs
+
+/Users/jacobmacritchie/CodeProjects/trpl-follow-along/enums/target/rls/debug/deps/enums-32cb1a8ee16c5c9a.d: src/main.rs
+
+src/main.rs:
