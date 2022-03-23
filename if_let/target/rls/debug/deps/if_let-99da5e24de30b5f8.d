6fca33bffecd1925bd8fa28cb2e4f1a468863d8e
Ajacmac <jacob.macritchie@gmail.com>
2022-03-22 21:01:28 -0300

    added initial if let example

---
+++ if_let/target/rls/debug/deps/if_let-99da5e24de30b5f8.d
@@ -0,0 +1,5 @@
+/Users/jacobmacritchie/CodeProjects/trpl-follow-along/if_let/target/rls/debug/deps/if_let-99da5e24de30b5f8.rmeta: src/main.rs
+
+/Users/jacobmacritchie/CodeProjects/trpl-follow-along/if_let/target/rls/debug/deps/if_let-99da5e24de30b5f8.d: src/main.rs
+
+src/main.rs:
---
+++ if_let/target/rls/debug/deps/if_let-9ab57b2d9e474d62.d
@@ -0,0 +1,5 @@
+/Users/jacobmacritchie/CodeProjects/trpl-follow-along/if_let/target/rls/debug/deps/if_let-9ab57b2d9e474d62.rmeta: src/main.rs
+
+/Users/jacobmacritchie/CodeProjects/trpl-follow-along/if_let/target/rls/debug/deps/if_let-9ab57b2d9e474d62.d: src/main.rs
+
+src/main.rs:
