78565953b28ccf9fadce4ea2644a9fee8b51487e
Ajacmac <jacob.macritchie@gmail.com>
2022-03-23 21:55:24 -0300

    added brief comment on if let since that section was brief

--- if_let/src/main.rs
+++ if_let/src/main.rs
@@ -1,3 +1,6 @@
+// if let is just a more concise way of writing a match statement with
+// a single conditional arm and a catch-all and do nothing arm
+
 fn main() {
     let config_max = Some(3u8);
     if let Some(max) = config_max {
