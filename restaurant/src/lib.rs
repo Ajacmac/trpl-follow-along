55a198b910e69da58f3827a9e59c2a2aef715e9d
Ajacmac <jacob.macritchie@gmail.com>
2022-03-26 00:08:47 -0300

    added use of enums, structs, and an additional module

--- restaurant/src/lib.rs
+++ restaurant/src/lib.rs
@@ -23,10 +23,34 @@ mod front_of_house {
     }
 }
 
+mod back_of_house {
+    pub enum Appetizer {
+        Soup,
+        Salad,
+    }
+
+    pub struct Breakfast {
+        pub toast: String,
+        seasonal_fruit: String,
+    }
+
+    impl Breakfast {
+        pub fn summer(toast: &str) -> Breakfast {
+            Breakfast {
+                toast: String::from(toast),
+                seasonal_fruit: String::from("peaches"),
+            }
+        }
+    }
+}
+
 pub fn eat_at_restaurant() {
     // Absolute path
     crate::front_of_house::hosting::add_to_waitlist();
 
     // Relative path
     front_of_house::hosting::add_to_waitlist();
+
+    let order1 = back_of_house::Appetizer::Soup;
+    let order2 = back_of_house::Appetizer::Salad;
 }
