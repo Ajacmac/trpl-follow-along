b885e43f51f7675ad537783370095ac1fbd2ab25
Ajacmac <jacob.macritchie@gmail.com>
2022-03-25 23:49:33 -0300

    added lib example and fleshed it out a bit

---
+++ restaurant/target/rls/debug/deps/restaurant-138449a8f4087725.d
@@ -0,0 +1,5 @@
+/Users/jacobmacritchie/CodeProjects/trpl-follow-along/restaurant/target/rls/debug/deps/restaurant-138449a8f4087725.rmeta: src/lib.rs
+
+/Users/jacobmacritchie/CodeProjects/trpl-follow-along/restaurant/target/rls/debug/deps/restaurant-138449a8f4087725.d: src/lib.rs
+
+src/lib.rs:
---
+++ restaurant/target/rls/debug/deps/restaurant-16d27a89b6cf8e4c.d
@@ -0,0 +1,5 @@
+/Users/jacobmacritchie/CodeProjects/trpl-follow-along/restaurant/target/rls/debug/deps/restaurant-16d27a89b6cf8e4c.rmeta: src/lib.rs
+
+/Users/jacobmacritchie/CodeProjects/trpl-follow-along/restaurant/target/rls/debug/deps/restaurant-16d27a89b6cf8e4c.d: src/lib.rs
+
+src/lib.rs:
