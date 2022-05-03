8ddd29b7e45720a658e3daaacfcc6a09ae940f65
Ajacmac <jacob.macritchie@gmail.com>
2022-05-03 18:35:12 -0400

    just learned nested match statements are a thing

--- errors/src/main.rs
+++ errors/src/main.rs
@@ -9,12 +9,19 @@
 // leaves all of the cleanup to the operating system to do.
 
 use std::fs::File;
+use std::io::ErrorKind;
 
 fn main() {
     let f = File::open("hello.txt");
 
     let f = match f {
         Ok(file) => file,
-        Err(error) => panic!("Error opening file: {:?}", error),
+        Err(error) => match error.kind() {
+            ErrorKind::NotFound => match File::create("hello.txt") {
+                Ok(fc) => fc,
+                Err(e) => panic!("Error creating the file: {:?}", e),
+            },
+            other_error => panic!("Prolem opening the file {:?}", other_error)
+        }
     };
 }
