8e6b468c52c6eecee73aee8f99a479f389a0e641
Ajacmac <jacob.macritchie@gmail.com>
2022-05-03 16:53:52 -0400

    added initial simplified error handling in error/panicking chapter

--- errors/src/main.rs
+++ errors/src/main.rs
@@ -8,6 +8,13 @@
 // The alternative is to simply abort on panic, which instead
 // leaves all of the cleanup to the operating system to do.
 
+use std::fs::File;
+
 fn main() {
-    panic!("Hello, world!");
+    let f = File::open("hello.txt");
+
+    let f = match f {
+        Ok(file) => file,
+        Err(error) => panic!("Error opening file: {:?}", error),
+    };
 }
