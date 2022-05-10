e12b841efda9a3c6ce5c48fd41d7a480e398db34
Ajacmac <jacob.macritchie@gmail.com>
2022-05-09 22:09:20 -0400

    first take on handling input validation with the type system

---
+++ type_based_input_validation/target/rls/debug/deps/type_based_input_validation-8b534494a2734fa2.d
@@ -0,0 +1,5 @@
+/Users/jacobmacritchie/CodeProjects/trpl-follow-along/type_based_input_validation/target/rls/debug/deps/type_based_input_validation-8b534494a2734fa2.rmeta: src/main.rs
+
+/Users/jacobmacritchie/CodeProjects/trpl-follow-along/type_based_input_validation/target/rls/debug/deps/type_based_input_validation-8b534494a2734fa2.d: src/main.rs
+
+src/main.rs:
---
+++ type_based_input_validation/target/rls/debug/deps/type_based_input_validation-ecb7b1db59ab4ab9.d
@@ -0,0 +1,5 @@
+/Users/jacobmacritchie/CodeProjects/trpl-follow-along/type_based_input_validation/target/rls/debug/deps/type_based_input_validation-ecb7b1db59ab4ab9.rmeta: src/main.rs
+
+/Users/jacobmacritchie/CodeProjects/trpl-follow-along/type_based_input_validation/target/rls/debug/deps/type_based_input_validation-ecb7b1db59ab4ab9.d: src/main.rs
+
+src/main.rs:
