749a4a45b6cd39febe300d1f67adb33a6bf6a006
Ajacmac <jacob.macritchie@gmail.com>
2022-05-11 22:57:43 -0400

    added initial example before using generics

---
+++ generics/target/rls/debug/deps/generics-1f44c1fdfd70ddf5.d
@@ -0,0 +1,5 @@
+/Users/jacobmacritchie/CodeProjects/trpl-follow-along/generics/target/rls/debug/deps/generics-1f44c1fdfd70ddf5.rmeta: src/main.rs
+
+/Users/jacobmacritchie/CodeProjects/trpl-follow-along/generics/target/rls/debug/deps/generics-1f44c1fdfd70ddf5.d: src/main.rs
+
+src/main.rs:
---
+++ generics/target/rls/debug/deps/generics-4329900335f1d2aa.d
@@ -0,0 +1,5 @@
+/Users/jacobmacritchie/CodeProjects/trpl-follow-along/generics/target/rls/debug/deps/generics-4329900335f1d2aa.rmeta: src/main.rs
+
+/Users/jacobmacritchie/CodeProjects/trpl-follow-along/generics/target/rls/debug/deps/generics-4329900335f1d2aa.d: src/main.rs
+
+src/main.rs:
